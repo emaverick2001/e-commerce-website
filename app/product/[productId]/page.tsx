2ff6d7b9c38ddbe1521ada70ddd0c62ae5a92b2a
Mav <mespin11@jh.edu>
2024-08-25 19:54:42 -0400

    fixed some basic styling

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -28,46 +28,9 @@ export default function Page({ params }: { params: { productId: string } }) {
           </div>
         </div>
         <hr className="my-10" />
-        Related Items
+        <div className="text-lg text-left">Similar Products</div>{" "}
       </div>
       <ProductList />
     </div>
   );
 }
-
-// const ProductPage = () => {
-//   // get the productId from the current page URL as it is the last
-//   //   const { productId } = router.;
-
-//   const url = window.location.href;
-//   const segments = url.split("/");
-//   const itemId = parseInt(segments[segments.length - 1]);
-
-//   const product = products.find((item) => item.id === Number(itemId));
-
-//   if (!product) {
-//     return <div>Product not found</div>;
-//   }
-
-//   return (
-//     <div className="bg-white">
-//       <div className="px-4 py-10 sm:px-6 lg:px-8">
-//         <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-//           <div className="product-card-container">
-//             <ProductCard item={product} />
-//           </div>
-//           <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-//             <h1>{product!.title}</h1>
-//             <p>{product!.description}</p>
-//             <p>{product!.price}</p>
-//           </div>
-//         </div>
-//         <hr className="my-10" />
-//         Related Items
-//       </div>
-//       <ProductList />
-//     </div>
-//   );
-// };
-
-// export default ProductPage;
